55579794c8de2b8536c6ec325eecc79159f26fb0
blahablast <blahasam@gmail.com>
2023-01-24 16:41:02 -0500

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by sam blaha on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
